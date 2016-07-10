2bdb6641ae24c02218631affcc6b6cba20857bb1
Zeke Hernandez <zeke.y.hernandez@gmail.com>
2016-07-10 18:09:26 -0500

    Implement basic arm functions.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -25,19 +25,45 @@ public class PlayerScript : MonoBehaviour {
 	private Character mCharacter;
 	private bool mIsButtonPressed;
 	private bool mIsJumping;
+	private bool mIsSwinging;
 	private Rigidbody2D rb2d;
+	private HingeJoint2D armJoint;
 
 	// Use this for initialization
 	void Start () {
 		mCharacter = new Character(TeamNum);
 		mIsJumping = false;
+		mIsSwinging = false;
 		
 		rb2d = GetComponent<Rigidbody2D>();
+		armJoint = transform.Find("Arm").transform.GetComponent<HingeJoint2D>();
+	}
+	
+	void SetArmSwing () {
+		JointAngleLimits2D limits = armJoint.limits;
+		JointMotor2D motor = armJoint.motor;
+		
+		limits.max = 220;
+		armJoint.limits = limits;
+		
+		armJoint.useMotor = true;
+		
+		mIsSwinging = true;	
+	}
+	
+	void SetArmRest() {
+		JointAngleLimits2D limits = armJoint.limits;
+		limits.max = 90;
+		armJoint.limits = limits;
+		armJoint.useMotor = false;
+		
+		mIsSwinging = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		bool isGrounded = Physics2D.Linecast(transform.position, GroundCheck.position,  1 << LayerMask.NameToLayer("Ground"));
+		bool shouldSwing = false;
 		
 		if (isGrounded) {
 			if (mIsButtonPressed && !mIsJumping) {
@@ -49,9 +75,17 @@ public class PlayerScript : MonoBehaviour {
 			if (mIsButtonPressed) {
 				if (mIsJumping) {
 					rb2d.AddForce(new Vector2(0, JumpStrength));
+				} else {
+					shouldSwing = true;	
 				}
 			} else if (mIsJumping) 
 				mIsJumping = false;
+		}
+		
+		if (mIsSwinging && !shouldSwing) {
+			SetArmRest();
+		} else if (!mIsSwinging && shouldSwing) {
+			SetArmSwing();
 		}
 	}
 
